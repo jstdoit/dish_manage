bd84c2d426f604871d332a298e17a52a8cbc6f98
ganggang <just_send@163.com>
2012-05-28 12:29:20 +0800

    init dish management pro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DishManage::Application.config.secret_token = '77d9da51a4bab673bcabb3c4bcda3ee6ad63584e222b330ad63a348d2b91b24cfa5d6052551c0237916eef126ffd547082e0f093be0c5c163f787f96c439e31d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DishManage::Application.config.session_store :cookie_store, :key => '_dish_manage_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DishManage::Application.config.session_store :active_record_store
