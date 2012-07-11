4e5378f8c637103425138a35f7bdeec93649fba1
Peng Yuan <pengpeng@SNDA-192-168-13-86.(none)>
2012-07-11 18:29:14 +0800

    完成APK上传功能

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,15 +1,26 @@
 class ClientController < ApplicationController
   def upload
-		uploaded_file = params[:apk_file]	
-		File.open(Rails.root.join('/swidy/client', '', uploaded_file.original_filename), 'wb') do |file|
-			file.write(uploaded_file.read)
-		end
+	uploaded_file = params[:apk_file]
+	versioncode = params[:vs]
+	versionnumber = params[:vn]
+	updateditems = params[:ui]
+
+	File.open(Rails.root.join('/swidy/client', '', uploaded_file.original_filename), 'wb') do |file|
+		file.write(uploaded_file.read)
+	end
+	File.open '/swidy/client/yuanpeng.txt', 'w' do |file|
+		file.puts "version code:" + versioncode
+
+		file.puts "version number:" + versionnumber
+
+		file.puts "update_items:" + updateditems
+	end
   end
 
   def index
-    respond_to do |format|
-      format.html # index.html.erb
-    end
+	respond_to do |format|
+		format.html # index.html.erb
+	end
 	
   end
 end
